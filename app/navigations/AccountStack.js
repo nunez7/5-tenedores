2a6587cb90d8e0658fba8a04030440acf70e0576
Felix Javier Nuñez Gonzalez <33491472+nunez7@users.noreply.github.com>
2022-06-27 16:10:29 -0600

    Screen de usuario no logeado

--- app/navigations/AccountStack.js
+++ app/navigations/AccountStack.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {createStackNavigator} from "@react-navigation/stack";
 import Account from "../screens/account/Account";
+import Login from "../screens/account/Login";
 
 const Stack = createStackNavigator();
 
@@ -10,6 +11,9 @@ export default function AccountStack(){
             <Stack.Screen name="account" 
             component={Account}
             options={{title: "Mi cuenta"}} />
+            <Stack.Screen name="login"
+            component={Login}
+            options={{title: "Iniciar sesión"}} />
         </Stack.Navigator>
     );
 }
--- app/screens/account/UserGuest.js
+++ app/screens/account/UserGuest.js
@@ -1,10 +1,54 @@
 import React from "react";
-import {View, Text} from "react-native";
+import {StyleSheet,View, ScrollView, Text, Image} from "react-native";
+import {Button} from "react-native-elements"
 
 export default function UserGuest(){
     return (
-        <View>
-            <Text>User Guest...</Text>
-        </View>
+        <ScrollView centerContent={true} style={styles.viewBody}>
+            <Image source={require("../../../assets/img/user-guest.jpg")}
+            resizeMode="contain" style={styles.image} />
+            <Text style={styles.title}>Consulta tu perfil de 5 Tenedores</Text>
+            <Text style={styles.description}>
+                ¿Cómo describirías tu mejor restaurante? Busca y visualiza los mejores restaurantes de una forma sencilla, vota cual te ha gustado más y comenta como ha sido tu experiencia.
+            </Text>
+            <View style={styles.viewBtn}>
+                <Button buttonStyle={styles.btnStyle}
+                containerStyle={styles.btnContainer} 
+                title="Ver tu perfil"
+                onPress={()=> console.log("Click")} />
+            </View>
+        </ScrollView>
     );
 }
+
+const styles = StyleSheet.create({
+    viewBody: {
+        marginLeft: 30,
+        marginRight: 30,
+    },
+    image: {
+        height: 300,
+        width: "100%",
+        marginBottom: 40
+    },
+    title:{
+        fontWeight: "bold",
+        fontSize: 19,
+        marginBottom: 10,
+        textAlign: "center"
+    },
+    description:{
+        textAlign: "center",
+        marginBottom: 20
+    },
+    viewBtn:{
+        flex: 1,
+        alignItems: "center"
+    },
+    btnStyle:{
+        backgroundColor: "#00a680",
+    },
+    btnContainer:{
+        width: "70%",
+    }
+});
