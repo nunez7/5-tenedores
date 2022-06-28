e4a3b345c3773aa0eece75c5848ef30e141637d3
Felix Javier Nuñez Gonzalez <33491472+nunez7@users.noreply.github.com>
2022-06-28 13:57:14 -0600

    Cerrando sesiones

--- app/screens/account/UserLogged.js
+++ app/screens/account/UserLogged.js
@@ -1,10 +1,23 @@
 import React from "react";
-import {View, Text} from "react-native";
+import { View, Text, Button } from "react-native";
+import { getAuth, signOut } from "firebase/auth";
 
-export default function UserLogged(){
-    return (
-        <View>
-            <Text>User Logged...</Text>
-        </View>
-    );
+export default function UserLogged() {
+  return (
+    <View>
+      <Text>User Logged...</Text>
+      <Button title="Cerrar sesión" onPress={() => cerrarSesion()} />
+    </View>
+  );
+}
+
+function cerrarSesion() {
+  const auth = getAuth();
+  signOut(auth)
+    .then(() => {
+      // Sign-out successful.
+    })
+    .catch((error) => {
+      // An error happened.
+    });
 }
