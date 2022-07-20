fb53b572c803f4bbb2c790332a784b8fff019c26
Felix Javier Nuñez Gonzalez <33491472+nunez7@users.noreply.github.com>
2022-07-20 09:37:37 -0600

    Crear restaurantes con validacion de sesion

---
+++ app/screens/restaurants/Restaurants.js
@@ -0,0 +1,51 @@
+import React, {useState, useEffect} from "react";
+import {StyleSheet, View, Text} from "react-native";
+import { Icon } from "react-native-elements";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+import {firebaseApp} from "../../utils/firebase";
+
+
+export default function Restaurants(){
+    const [user, setUser] = useState(null);
+    //verificamos el estado del user
+    useEffect(() => {
+        const auth = getAuth();
+        const user = auth.currentUser;
+        onAuthStateChanged(auth, (user) => {
+          //console.log(user);
+          !user ? setUser(false) : setUser(true);
+        });
+      }, []);
+    
+
+    return (
+        <View style={styles.viewBody}>
+            <Text>Restaurants...</Text>
+            {user && (
+            <Icon
+                reverse
+                type="material-community"
+                name="plus"
+                color="#00a680"
+                containerStyle={styles.btnContainer}
+            />
+            )}
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    viewBody:{
+        flex: 1,
+        backgroundColor: "#FFF",
+    },
+    btnContainer: {
+        position: "absolute",
+        bottom: 10,
+        right: 10,
+        shadowColor: "black",
+        shadowOffset:{width: 2, height: 2},
+        shadowOpacity: 0.5,
+    }
+});
